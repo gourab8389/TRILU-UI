c7afe23875231257b4903ea25644910e39706ae5
gourab8389 <deyg6988@gmail.com>
2024-07-05 16:28:54 +0530

    NextSSRPlugin  & productFileUpload

--- app/sell/page.tsx
+++ app/sell/page.tsx
@@ -1,67 +1,81 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import React from 'react'
 import SelectCategory from '../components/SelectCategory'
 import { Textarea } from '@/components/ui/textarea'
 import { TipTapEditor } from '../components/Editor'
+import { UploadDropzone } from '../lib/uploadthing'
+import { Button } from '@/components/ui/button'
 
 function SellRoute() {
-  return (
-   <section className='max-w-7xl mx-auto md:px-8 mb-14'>
-    <Card>
-        <form >
-            <CardHeader>
-                <CardTitle>
-                    Sell your product with ease
-                </CardTitle>
-                <CardDescription>
-                    Please describe your product here in details so that it can be sold
-                </CardDescription>
-            </CardHeader>
+    return (
+        <section className='max-w-7xl mx-auto md:px-8 mb-14'>
+            <Card>
+                <form >
+                    <CardHeader>
+                        <CardTitle>
+                            Sell your product with ease
+                        </CardTitle>
+                        <CardDescription>
+                            Please describe your product here in details so that it can be sold
+                        </CardDescription>
+                    </CardHeader>
 
 
-            <CardContent className='flex flex-col gap-y-10'>
-                <div className="flex flex-col gap-y-2">
-                    <Label>
-                        Name
-                    </Label>
-                    <Input type='text' placeholder='Name of your Product'/>
-                </div>
+                    <CardContent className='flex flex-col gap-y-10'>
+                        <div className="flex flex-col gap-y-2">
+                            <Label>
+                                Name
+                            </Label>
+                            <Input type='text' placeholder='Name of your Product' />
+                        </div>
 
 
-                <div className="flex flex-col gap-y-2">
-                    <Label>Category</Label>
-                    <SelectCategory/>
-                </div>
+                        <div className="flex flex-col gap-y-2">
+                            <Label>Category</Label>
+                            <SelectCategory />
+                        </div>
 
 
 
-                <div className="flex flex-col gap-y-2">
-                    <Label>Price</Label>
-                    <Input type='number' placeholder=' ₹ 99.00' />
-                </div>
+                        <div className="flex flex-col gap-y-2">
+                            <Label>Price</Label>
+                            <Input type='number' placeholder=' ₹ 99.00' />
+                        </div>
 
 
-                <div className="flex flex-col gap-y-2">
-                    <Label>Small Summary</Label>
-                    <Textarea placeholder='Please describe your product shortly right here...'/>
-                </div>
+                        <div className="flex flex-col gap-y-2">
+                            <Label>Small Summary</Label>
+                            <Textarea placeholder='Please describe your product shortly right here...' />
+                        </div>
 
 
-                <div className="flex flex-col gap-y-2">
-                    <Label>
-                        Description
-                    </Label>
-                    <TipTapEditor/>
-                 </div>
+                        <div className="flex flex-col gap-y-2">
+                            <Label>
+                                Description
+                            </Label>
+                            <TipTapEditor />
+                        </div>
 
+                        <div className="flex flex-col gap-y-2">
+                            <Label>Product Images</Label>
+                            <UploadDropzone endpoint='imageUploader' />
+                        </div>
 
-            </CardContent>
-        </form>
-    </Card>
-   </section>
-  )
+                        <div className="flex flex-col gap-y-2">
+                        <Label>Product File</Label>
+                        <UploadDropzone endpoint='productFileUpload'/>
+                        </div>
+
+                    </CardContent>
+                    <CardFooter className='mt-5'>
+                        <Button>Submit Button</Button>
+                    </CardFooter>
+                </form>
+            </Card>
+        </section>
+    )
 }
 
 export default SellRoute

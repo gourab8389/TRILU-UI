1473381b20b5b5b96aed76671b2a34a60aba4256
gourab8389 <deyg6988@gmail.com>
2024-07-05 14:47:12 +0530

    editor

--- app/sell/page.tsx
+++ app/sell/page.tsx
@@ -3,10 +3,12 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import React from 'react'
 import SelectCategory from '../components/SelectCategory'
+import { Textarea } from '@/components/ui/textarea'
+import { TipTapEditor } from '../components/Editor'
 
 function SellRoute() {
   return (
-   <section className='max-w-7xl mx-auto md:px-8'>
+   <section className='max-w-7xl mx-auto md:px-8 mb-14'>
     <Card>
         <form >
             <CardHeader>
@@ -28,6 +30,23 @@ function SellRoute() {
                     <Label>Category</Label>
                     <SelectCategory/>
                 </div>
+
+                <div className="flex flex-col gap-y-2">
+                    <Label>Price</Label>
+                    <Input type='number' placeholder=' ₹ 99.00' />
+                </div>
+                <div className="flex flex-col gap-y-2">
+                    <Label>Small Summary</Label>
+                    <Textarea placeholder='Please describe your product shortly right here...'/>
+                </div>
+
+                <div className="flex flex-col gap-y-2
+                 ">
+                    <Label>
+                        Description
+                    </Label>
+                    <TipTapEditor/>
+                 </div>
             </CardContent>
         </form>
     </Card>
